bddbca14a67171b34d27ca20b16f1824aaa7ff05
DmitryMurashov <dmtr.murashov@gmail.com>
2021-08-15 15:24:24 +0300

    [storage] CSV | Search functionality, file operations
    - Reimplemented the way it works with files. Now, the files get opened on-demand, meaning that the instance of a Csv object does not keep ownership
    - Implemented crude substring search functionality

--- storage.py
+++ storage.py
@@ -10,26 +10,42 @@ class Csv:
 	def __init__(self, filename):
 		_f_write_header = not os.path.isfile(filename)
 
-		self._field_names = ["date", "lang_target", "lang_origin"]
-		self._file = open(filename, "a+")
-		self._file_close_callback = lambda f: f.close()
+		self.filename = filename
+		self._field_names = ["date", "lang1", "lang2"]
 		self._date = datetime.datetime.strftime(datetime.datetime.now(), _DATETIME_FORMAT)
 
-		if _f_write_header:
-			csv.DictWriter(self._file, self._field_names).writeheader()
+		if not os.path.isfile(self.filename):
+			with open(self.filename, "a+") as file_instance:
+				csv.DictWriter(file_instance, self._field_names).writeheader()
 
-	def __del__(self):
-		self._file_close_callback
+	def write(self, lang1: str, lang2: str):
+		with open(self.filename, "a+") as file_instance:
+			writer = csv.DictWriter(file_instance, fieldnames=self._field_names)
+			writer.writerow({
+				"date": self._date,
+				"lang1": lang1,
+				"lang2": lang2
+			})
 
-	def write(self, target: str, origin: str):
-		writer = csv.DictWriter(self._file, self._field_names)
-		writer.writerow({
-			"date": self._date,
-			"lang_origin": origin,
-			"lang_target": target
-		})
+	def find(self, query):
+		"""
+		Search for a substring ignoring case
+		:param query: substring
+		:return: list of tuples: [(lang1, lang2)]
+		"""
+		results = []
+
+		with open(self.filename, "r") as file_instance:
+			reader = csv.reader(file_instance)
+			for _, lang1, lang2 in reader:
+				print(lang2.lower())
+				if lang1.lower().find(query.lower()) or lang2.lower().find(query.lower()):
+					results += [(lang1, lang2)]
+
+		return results
 
 
 if __name__ == "__main__":
 	s = Csv("storage.csv")
 	s.write("echo", "well, very well")
+	print(s.find("very"))
