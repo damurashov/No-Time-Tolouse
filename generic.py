5299e7f4622d6aba4e4971db6b99b6a0e406f335
DmitryMurashov <dmtr.murashov@gmail.com>
2021-08-15 14:42:43 +0300

    [generic] Output | Basic console I/O operations

---
+++ generic.py
@@ -0,0 +1,46 @@
+import subprocess
+import shlex
+
+def command_run(command, verbose=True):
+	command = shlex.split(command)
+	if not verbose:
+		return subprocess.Popen(command, stdout=subprocess.PIPE)
+	else:
+		return subprocess.Popen(command)
+
+
+def command_output(command, verbose=False) -> str and int:
+	"""
+	:param command: command to execute
+	:return: console output, string
+	"""
+	proc = command_run(command, verbose)
+	proc.wait()
+
+	if verbose:
+		return "", proc.returncode
+
+	return proc.stdout.read().decode("unicode_escape"), proc.returncode
+
+
+def dialog_yn(text) -> bool:
+	while True:
+		ans = input(text)
+		if 'y' in ans and 'n' not in ans:
+			print('y')
+			return True
+		elif 'n' in ans and 'y' not in ans:
+			print('n')
+			return False
+
+
+def smart_split(string):
+	NEWLINE = '\n'
+
+	string = string.strip()
+	hist = Counter(string)
+
+	if NEWLINE in hist:
+		return string.split(NEWLINE)
+	else:
+		return re.split('\s+', string)
